3f0cbb685deb9ac4796fc5731638da9b5a131c3d
jackwagneruni <jackwagneruni@gmail.com>
2025-01-17 07:07:37 +0000

    had to sleep, it

---
+++ bubbles_and_pointers.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+
+void printValues(int*);
+//void sort(int*);
+//void swap(int*, int*);
+
+
+void printValues (int values[]) {
+        printf("[");
+        for (int i = 0; i < 9; i++){
+                printf("%d", values[i]);
+                printf(" ");
+        }
+        printf("]");
+}
+
+int main(){
+  int values[] = {7, 3, 9, 4, 6, 1, 2, 8, 5};
+  printf("Before: \n");
+  printValues(values);
+/*
+  // test swap
+  int x = 3;
+  int y = 5;
+  printf("x: %d, y: %d \n", x, y);
+  swap(&x, &y);
+  printf("x: %d, y: %d \n", x, y);
+
+  sort(values);
+  printf("After: \n");
+  printValues(values);
+  */
+  printf("\n");
+  return(0);
+  
+} // end main
